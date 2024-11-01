a6a3a9570f51a67b7cf44e1b0a72b90dfa5f8b58
Angel de Jesus Rufino Mendoza <230499@utxicotepec.edu.mx>
2024-11-01 10:36:00 -0600

    Practica 09
    Se agregaron los archivos index.js, saluda.js, inicialización del proyecto y dependencias de desarrollo

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express'); //DECLARANDO UN OBJETO QUE VA A PERMITIR LEER PAGINAS ETC.importar la libreria para crear un servidor web
+//INSTANCIAR NUESTRA APLICACIÓN WEB
+const app = express();
+//CONFIGURAMOS NUESTRO SERVIDOR WEB (puerto donde estara escuchando nuestro sitio web)
+const port = 3000;
+app.listen(port, () => {
+  console.log(`La aplicación ha iniciado en el puerto: ${port}`);  
+});
+//Probamos rutas para poder presentar mensajes al usuario a través del navegador
+app.get("/",function(req, res){
+    res.send("Hola mundo desde Node, a través del navegador")
+});
+app.get("/QuienSoy",function(req, res){
+    res.json({"Estudiante": "Guadalupe Idai Vargas Galindo",
+        "grado":"4°",
+        "grupo":"B",
+        "asignatura":"Aplicaciones de Software Orientado a Servicios (AWOS)"});
+    })
