db441f74b0d5d9d2314d9432eb341205070ea959
Angel de Jesus Rufino Mendoza <230499@utxicotepec.edu.mx>
2024-11-08 11:30:46 -0600

    Práctica 12
    Actualización de la paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ export default {
   content: ['./views/**/*.pug'],
   theme: {
     extend: {},
-    colors:{
+    colors: {
       transparent: 'transparent',
-      current:'currentcolor',
+      current: 'currentcolor',
       'black': '#000000',
       'white': '#ffffff',
-      'celadon':'#A8E6A1',
-      'Esmerald':'#50c878',
-      'pigmentgreen':'#4CAF50',
+      'celadon': '#A8E6A1',
+      'emerald': '#50c878', // Cambiado a lowercase para consistencia
+      'pigmentgreen': '#4CAF50',
     },
   },
   plugins: [],
